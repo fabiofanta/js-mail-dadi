a72d2f2dabae58cc13551d2e132b436d08f7ab3f
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-02-20 11:29:35 +0100

    5-rel

--- js/main.js
+++ js/main.js
@@ -3,14 +3,29 @@
 // controlla se esiste un email uguale nell'array
 // se esiste, scrivi email confermata
 
+// var listaEmail = ["d.n@gmail.com","f.t@gmail.com","w.t@gmail.com","y.u@gmail.com", "r.t@hotmail.it"];
+// console.log(listaEmail);
+// var emailUtente = prompt('Scrivi la tua email per vedere se è in lista');
+// console.log(emailUtente);
+// for (var i = 0; i < listaEmail.length; i++) {
+//     if (emailUtente == listaEmail[i]) {
+//         console.log("La tua email è presente");
+//     } else {
+//         console.log("La tua email non è presente");
+//     }
+// }
+
 var email = ["d.n@gmail.com","f.t@gmail.com","w.t@gmail.com","y.u@gmail.com", "r.t@hotmail.it"]
 var emailInput = prompt("Scrivi la tua email");
-for (var i = 0; i < email.length; i++) {
-    var emailCheck = emailInput == email[i];
-}
+// for (var i = 0; i < email.length; i++) {
+//     if (emailInput == listaEmail[i]) {
+//         entra = true;
+//     } else {
+//
+//     }
 
-if (emailCheck == true) {
-    console.log("Email Confermata");
-} else {
-    console.log("Email Invalida");
-}
+    if (email.includes(emailInput)) {
+        console.log("Sei in lista");
+    } else {
+        console.log("Non sei in lista");
+    }
--- js/mainDadi.js
+++ js/mainDadi.js
@@ -1,17 +1,17 @@
-var random1 = Math.floor(Math.random() * 10) + 1;
-var random2 = Math.floor(Math.random() * 10) + 1;
+var random1 = Math.floor(Math.random() * 6) + 1;
+var random2 = Math.floor(Math.random() * 6) + 1;
 console.log(random1);
 console.log(random2);
-document.getElementById('rd-1').innerHTML = "random1= " + random1
-document.getElementById('rd-2').innerHTML = "random2= " + random2
+document.getElementById('rd-1').innerHTML = "random1= " + random1;
+document.getElementById('rd-2').innerHTML = "random2= " + random2;
 if (random1 > random2) {
     console.log("random1 ha vinto");
-    document.getElementById('result').innerHTML = "random1 ha vinto"
+    document.getElementById('result').innerHTML = "random1 ha vinto";
 } else if (random1 < random2) {
     console.log("random2 ha vinto");
-    document.getElementById('result').innerHTML = "random2 ha vinto"
+    document.getElementById('result').innerHTML = "random2 ha vinto";
 } else {
     console.log("random1 e random2 hanno pareggiato");
-    document.getElementById('result').innerHTML = "random1 e random2 hanno pareggiato"
+    document.getElementById('result').innerHTML = "random1 e random2 hanno pareggiato";
 
 }
